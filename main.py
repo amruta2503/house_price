cecdf86e29f3ddc28e9ad0ca30c16ae9fa543d98
amruta2503 <142072684+amruta2503@users.noreply.github.com>
2024-02-14 22:03:38 -0800

    entity_config

---
+++ main.py
@@ -0,0 +1,5 @@
+from source.entity.config_entity import TrainingPipelineCongig
+
+if __name__ == '__main__':
+    train_pipeline_config_obj = TrainingPipelineCongig()
+    print(train_pipeline_config_obj.__dict__)
---
+++ source/constant/constant_train.py
@@ -0,0 +1,16 @@
+TARGET_COLUMN = "SalePrice"
+TRAIN_PIPELINE_NAME = "training_pipeline"
+ARTIFACT_DIR = "artifact"
+FILE_NAME = "training_data.csv"
+
+TRAIN_FILE_NAME = "train.csv"
+TEST_FILE_NAME = "test.csv"
+
+MONGODB_URL_KEY = ""
+DATABASE_NAME = "db-house-price"
+
+DI_COLLECTION_NAME = "house-saleprice"
+DI_DIR_NAME = "data_ingestion"
+DI_FEATURE_STORE_DIR = "feature_store"
+DI_INGESTED_DIR = "ingested"
+DI_TRAIN_TEST_SPLIT_RATIO = 0.2
